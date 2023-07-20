c5f6eefb4a72bab3532c18eb4035c1022aa034c1
CODERx <sarthakbhatt0@gmail.com>
2023-07-20 17:10:46 +0530

    m

--- LEARNING/script.js
+++ LEARNING/script.js
@@ -1,3 +1,14 @@
 // alert('JS file is LINKED !!!')
 // console.log('hello JS !!!')
-console.log("message is printed");
+// console.log("message is printed");
+
+// let myName = "Bhatt";
+
+// function showMyName(){
+//     let myName = "Bhatt";
+//     alert(myName)
+//     printAStatementWithMyName(myName)
+// }
+// function printAStatementWithMyName(myName){
+//     alert("HEY "+myName+", HOW ARE YOU");
+// }
